2194caca583f62962901a2f68ddf4a9ebc3b6ac2
jlemosmusi <lemos.ema@gmail.com>
2022-04-28 22:00:03 -0300

    nuevo

--- back_AB/microservicios/reservation/routes/get_data_host_from_id.py
+++ back_AB/microservicios/reservation/routes/get_data_host_from_id.py
@@ -33,26 +33,84 @@ data_particular_house=Blueprint("data_particular_house",__name__)
 # data=verificarSiEstaDisponible(diasBuscados('2022-3-23','2022-3-24'),"PERRO","Buenos Aires")
 
 
+# firma respuesta
+# {
+#     "house_id":"1",
+#     "name_house":"juancito casa",
+#     "adress":"esteban de luca 1851",
+#     "type_of_house":"ph",
+#     "anfitrion_name":"juan carlos",
+#     "tipo de animales que admite":["perros","gatos","caballos"],
+#     "amenities":['jardin',"balcon"],
+#     "images":[],
+#     "reviews_comment":["no me gusto","me gusto mucho"],
+#     "calification":"(2,10)",
+#     "calificationAverage":"6",
+#     "description":"es un ph muy grande , tiene patio y me dedico hace mucho a esto",
+#     "beed": [ 
+#                 {
+#                     "beed_id":"2",
+#                     "services":["Comida","paseo","bano"],
+#                     "beed_name":"privado",
+#                     "tipo_animal":"perro"
+#                 }
+#             ]
+# }
+
+
+house_id="12"
+
+
+
+def house_info(house_id):
+    house_id,avatar_info_id,house_name,country_id,city_id,district_id,cp_id,street,description,email,phone,mobile_phone,lat,long=buscar_id('house_info','*','id',house_id)
+    country_name=buscar_id('countries','name','id',str(country_id))
+    city_name=buscar_id('city','name','id',str(city_id))
+    district_name=buscar_id('district','name','id',str(district_id))
+    cp_name=buscar_id('cp','name','id',str(cp_id))
+    adrees_name= country_name+", "+city_name+", "+district_name+", "+cp_name+", "+street
+    
+
+
+
+
+
 def dataParticularHouse(house_id):
     houses={}
+    house_id=house_id
+    house_adress:
     name_house=buscar_id('house_info','name','id',house_id)
     images_id=buscar_id('home_info_images','images_id','house_info_id',house_id)
     # si tiene imagen
     if images_id:
-        image=buscar_id('images','name','id',str(images_id))
+        if type(images_id)==tuple:
+            image=buscar_id('images','name','id',images_id)
+        else:
+            image=buscar_id('images','name','id',images_id)
+
     else:
         image=[]
+
     # busco los comentarios y las calificaciones
     reviews_id=buscar_id('house_info_reviews','reviews_id','house_info_id',house_id)
+    # voy por el promedio de las calificaciones
     calification=[]
     calificationAverage=[]
+    # voy con los comentarios
+    reviews_comment=[]
     if reviews_id:
         if type(reviews_id)!= tuple:
             reviews_id=reviews_id,
         for reviews in reviews_id:
             calification.append(float(buscar_id('reviews','calification','id',str(reviews))))
+            reviews_comment.append(float(buscar_id('reviews','description','id',str(reviews))))
+
         
         calificationAverage=sum(calification)/len(calification)  
+
+
+    
+    
 
         # voy a los servicios por cama = e
     for e in house_id_and_beed_id[i]:
